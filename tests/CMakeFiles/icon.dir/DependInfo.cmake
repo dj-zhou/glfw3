b5b608b5950814000807e7b7dfc3ea56660c4be1
Dingjaing Zhou <zhoudingjiang@gmail.com>
2019-02-20 09:56:15 -0500

    initial push

---
+++ tests/CMakeFiles/icon.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/iot01/yugong_installer/third_party/glfw/deps/glad.c" "/home/iot01/yugong_installer/third_party/glfw/tests/CMakeFiles/icon.dir/__/deps/glad.c.o"
+  "/home/iot01/yugong_installer/third_party/glfw/tests/icon.c" "/home/iot01/yugong_installer/third_party/glfw/tests/CMakeFiles/icon.dir/icon.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GLFW_DLL"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "deps"
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/iot01/yugong_installer/third_party/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
